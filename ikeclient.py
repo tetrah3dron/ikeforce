02be8f17c814386a1cf3052f317e68fc25bdb3a9
kuroik3nshi <151956971+kuroik3nshi@users.noreply.github.com>
2025-09-21 18:31:09 +0300

    Update ikeclient.py
    fixed issues, see comments

--- ikeclient.py
+++ ikeclient.py
@@ -5,7 +5,10 @@
 #This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 #You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import socket,OpenSSL,sys,struct
+import sys, os, socket
+import struct
+import OpenSSL
+
 try:
         import udp
 except:
@@ -43,7 +46,7 @@ class IKEv1Client(object):
     def secRandom(self, bytes):
 	#Creates selected number of random bytes
 	#Provide the number of bytes required as input, method will return raw bytes
-	randomBytes = OpenSSL.rand.bytes(bytes)
+	randomBytes = os.urandom(bytes) # https://github.com/SpiderLabs/ikeforce/pull/11/commits/bc5fa4a4b99b02c22289e981050d70bf947a916f
 	return randomBytes
 
     def payBuild(self, strPayload, lenLen, *arg):
